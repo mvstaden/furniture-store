1a053ba982c7bd94d212dabe32ad82034cf62a06
Marius Van Staden <mvstaden91@gmail.com>
2024-09-12 16:17:55 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  About,
+  Cart,
+  Checkout,
+  Login,
+  Register,
+  Products,
+  SingleProduct,
+  Orders,
+} from "./pages";
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+export default App;
